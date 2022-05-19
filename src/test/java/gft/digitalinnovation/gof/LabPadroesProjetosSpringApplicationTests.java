2866d6b42a232bc105335c3a137605ac62b23d01
JadeRios <rios.jade@gmail.com>
2022-05-19 11:38:00 -0300

    Desafio Padroes de Projeto - GFT Start Woman

---
+++ src/test/java/gft/digitalinnovation/gof/LabPadroesProjetosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gft.digitalinnovation.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
